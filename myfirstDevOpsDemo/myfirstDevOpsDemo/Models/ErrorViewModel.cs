6482df15e84b441d93588460c91a710dbda3e2a2
Shobhit-SJ <contactshobhitjaiswal@gmail.com>
2024-10-17 14:28:54 +0530

    very first commit

---
+++ myfirstDevOpsDemo/myfirstDevOpsDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace myfirstDevOpsDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
